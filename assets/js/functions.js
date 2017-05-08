4afb615fa598f209c236842e47dd2464d6554d54
LeNPaul <l.nguyen.paul@gmail.com>
2017-05-08 02:10:27 -0400

    Test

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -51,53 +51,6 @@ function workLoad() {
   });
 }
 
-function clientStuff() {
-
-  $('.client-logo, .client-button').click(function() {
-    var $this = $(this),
-        position = $this.parent().children().index($this);
-
-    $('.client-unit').removeClass('active-client').eq(position).addClass('active-client');
-    $('.client-logo').removeClass('active-client').eq(position).addClass('active-client');
-    $('.client-button').removeClass('active-client').eq(position).addClass('active-client');
-  });
-
-
-  $('.client-control-next, .client-control-prev').click(function() {
-
-    var $this = $(this),
-        curActiveClient = $('.clients-belt').find('.active-client'),
-        position = $('.clients-belt').children().index(curActiveClient),
-        clientNum = $('.client-unit').length;
-
-      if($this.hasClass('client-control-next')) {
-
-        if(position < clientNum -1){
-          $('.active-client').removeClass('active-client').next().addClass('active-client');
-        } else {
-          $('.client-unit').removeClass('active-client').first().addClass('active-client');
-          $('.client-logo').removeClass('active-client').first().addClass('active-client');
-          $('.client-button').removeClass('active-client').first().addClass('active-client');
-        }
-
-      } else {
-
-        if (position === 0) {
-          $('.client-unit').removeClass('active-client').last().addClass('active-client');
-          $('.client-logo').removeClass('active-client').last().addClass('active-client');
-          $('.client-button').removeClass('active-client').last().addClass('active-client');
-        } else {
-          $('.active-client').removeClass('active-client').prev().addClass('active-client');
-        }
-
-      }
-
-
-  });
-
-}
-
-
 (function( $ ){
 
   $.fn.fitText = function( kompressor, options ) {
